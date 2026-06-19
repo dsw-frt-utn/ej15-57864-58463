4c41a97ffc8d1f07b6942d37cd6e5274c8c24391
Martina Lopez <martinalopezagueroo@gmail.com>
2026-06-18 23:13:54 -0300

    program

--- Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Program.cs
@@ -1,32 +1,26 @@
+using Dsw2026Ej15.Domain.Interfaces;
+using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Api.Middlewares;
 
-namespace Dsw2026Ej15.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseAuthorization();
+var builder = WebApplication.CreateBuilder(args);
 
 
-            app.MapControllers();
+builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
 
-            app.Run();
-        }
-    }
-}
+builder.Services.AddControllers();
+
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+
+app.MapHealthChecks("/health-check");
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
