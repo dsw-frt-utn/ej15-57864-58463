e6f508f259bc2b3bae8d2e681e52df05cfae581a
Martina Lopez <martinalopezagueroo@gmail.com>
2026-06-18 21:06:06 -0300

    clases y carpetas creadas

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    internal class Doctor
+    {
+    }
+}
